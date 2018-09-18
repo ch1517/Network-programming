6da1a3dfa8c522f3eaa4b91d044f8a9ade4b37c4
JeongSuHyeon <25702775+ch1517@users.noreply.github.com>
2018-09-18 22:10:08 +0900

    절대JAVA Programming  Chapter2

--- Chapter2/src/Chpater2_10/NumberTable.java
+++ Chapter2/src/Chpater2_10/NumberTable.java
@@ -5,7 +5,7 @@ public class NumberTable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=15;i++) { // 1부터 15까지 i 1씩 증가
-			System.out.printf("8진수: %o, 10진수 : %d, 16진수 : %x\n",i,i,i);
+			System.out.format("8진수:%1$o, 10진수 : %1$d, 16진수 : %1$x %n",i);
 		}
 	}
 
